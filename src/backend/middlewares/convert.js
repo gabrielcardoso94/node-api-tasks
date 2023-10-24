4ad872a402b91486b84184ff7ac592a60b1ce133
Gabriel Silva <gabriel.vcsilva@outlook.com>
2023-10-23 23:20:19 -0300

    Implementado validacoes nas rotas POST e PUT

--- src/backend/middlewares/convert.js
+++ src/backend/middlewares/convert.js
@@ -2,18 +2,12 @@ export async function convertStream(request) {
     const content = []
     
     for await (let chunk of request) {
-        content.push(chunk)
+        content.push(chunk) 
     }
 
-    try {
-        
-        const contentJson = content.join(",")
-        
-        if(contentJson) {
-            request.body = JSON.parse(contentJson)
-        }
-    }
-    catch (err) {
-        console.log(err.message)
+    if(content.length) {
+        const contentJson = JSON.parse(content.join(","))
+
+        request.body = contentJson
     }
 }
--- src/backend/routes.js
+++ src/backend/routes.js
@@ -19,38 +19,50 @@ export const routes = [
         method: 'POST',
         path: regex('/tasks'),
         handle: (request, response) => {
-            const {title, description} = request.body
-            const {url} = request
-            const table = url.replace("/", "")
-            const task = new Task(title, description)
             
-            tasks.create(table, task)
-
-            response.writeHead(201, {'Content-Type': 'application/json'})
-            response.end(JSON.stringify(task))
+            if(request.body && request.body.title.length > 0 && request.body.description.length > 0) {
+                const {title, description} = request.body
+                const {url} = request
+                const table = url.replace("/", "")
+                const task = new Task(title, description)
+                    
+                tasks.create(table, task)
+                        
+                response.writeHead(201, {'Content-Type': 'application/json'})
+                response.end(JSON.stringify(task))
+            } else {
+                response.writeHead(400)
+                response.end("Task inválida")
+            }
         }
     },
     {
         method: 'PUT',
         path: regex('/tasks/:id'),
         handle: (request, response) => {
-            const {url} = request
-            const {id} = request.params
-            const table = url.match('(?<table>[a-z]+)').groups.table
-            const task = request.body
-            task.updated_at = new Date().toISOString()
+            
+            if(request.body && request.body.title.length > 0 && request.body.description.length > 0) {
+                const {url} = request
+                const {id} = request.params
+                const table = url.match('(?<table>[a-z]+)').groups.table
+                const task = request.body
+                task.updated_at = new Date().toISOString()
 
-            const result = tasks.update(table, id, task)
+                const result = tasks.update(table, id, task)
 
-            if(result) {
-                response.writeHead(204)
-                response.end()
+                if(result) {
+                    response.writeHead(204)
+                    response.end()
+                } else {
+                    response.writeHead(400)
+                    response.end("ID não encontrado")
+                }
+
             } else {
                 response.writeHead(400)
-                response.end("ID não encontrado")
+                response.end("Task inválida")
             }
-
-            
+                        
         }
     },
     {
